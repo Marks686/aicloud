08c6b8b673891e584a63130547372234c033416e
Marks686 <17829791810@163.com>
2024-06-27 00:15:38 +0800

    OpenAI聊天功能前后端互通 补充绘图功能

--- aicloud/src/main/java/com/example/aicloud/controller/OpenAIController.java
+++ aicloud/src/main/java/com/example/aicloud/controller/OpenAIController.java
@@ -2,7 +2,12 @@ package com.example.aicloud.controller;
 
 import com.example.aicloud.util.ResponseEntity;
 import jakarta.annotation.Resource;
+import org.springframework.ai.image.ImageOptions;
+import org.springframework.ai.image.ImagePrompt;
+import org.springframework.ai.image.ImageResponse;
 import org.springframework.ai.openai.OpenAiChatModel;
+import org.springframework.ai.openai.OpenAiImageModel;
+import org.springframework.ai.openai.OpenAiImageOptions;
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,6 +17,8 @@ import org.springframework.web.bind.annotation.RestController;
 public class OpenAIController {
     @Resource
     private OpenAiChatModel chatModel;
+    @Resource
+    private OpenAiImageModel imageModel;
     /**
      * 调用OpenAI 聊天接口
      * @param question
@@ -20,10 +27,22 @@ public class OpenAIController {
     @RequestMapping("/chat")
     public ResponseEntity chat(String question) {
         if (!StringUtils.hasLength(question)){
+            // 输入为空
             return ResponseEntity.fail("问题不能为空");
         }
         // 调用OpenAI 接口
         String result = chatModel.call(question);
         return ResponseEntity.succ(result);
+    }
+
+    @RequestMapping("/draw")
+    public ResponseEntity draw(String question) {
+        if (!StringUtils.hasLength(question)){
+            // 输入为空
+            return ResponseEntity.fail("问题不能为空");
+        }
+        // 调用OpenAI 接口
+        ImageResponse result = imageModel.call(new ImagePrompt(question));
+        return ResponseEntity.succ(result.getResults().get(0));
     }
 }
