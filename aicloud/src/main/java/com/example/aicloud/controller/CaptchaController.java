025a067bf8a17f77acf6585feba715bb54bcdb51
Marks686 <17829791810@163.com>
2024-06-30 16:46:05 +0800

    添加Redis实现动态验证码判断

--- aicloud/src/main/java/com/example/aicloud/controller/CaptchaController.java
+++ aicloud/src/main/java/com/example/aicloud/controller/CaptchaController.java
@@ -3,10 +3,14 @@ package com.example.aicloud.controller;
 import cn.hutool.captcha.CaptchaUtil;
 import cn.hutool.captcha.LineCaptcha;
 import cn.hutool.core.net.NetUtil;
+import cn.hutool.crypto.SecureUtil;
+import cn.hutool.http.server.HttpServerRequest;
 import com.example.aicloud.util.MinIoUtil;
 import com.example.aicloud.util.ResponseEntity;
 import io.minio.errors.*;
 import jakarta.annotation.Resource;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,6 +19,8 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
+import java.security.Security;
+import java.util.concurrent.TimeUnit;
 
 @RestController
 @RequestMapping("/captcha")
@@ -22,17 +28,21 @@ public class CaptchaController {
 
     @Resource
     private MinIoUtil minIoUtil;
+    @Resource
+    private RedisTemplate redisTemplate;
     /**
      * 生成验证码
      */
     @RequestMapping("/create")
-    public ResponseEntity createCaptcha() throws ServerException, InsufficientDataException, ErrorResponseException, IOException, NoSuchAlgorithmException, InvalidKeyException, InvalidResponseException, XmlParserException, InternalException {
+    public ResponseEntity createCaptcha(HttpServletRequest request) throws ServerException, InsufficientDataException, ErrorResponseException, IOException, NoSuchAlgorithmException, InvalidKeyException, InvalidResponseException, XmlParserException, InternalException {
         String url = "";
         // 定义图形验证码的长和宽
         LineCaptcha lineCaptcha = CaptchaUtil.createLineCaptcha(120, 40);
-        String fileName = "captcha-" + NetUtil.getLocalhost().toString();
+        String fileName = "captcha-" + SecureUtil.md5(request.getRemoteAddr());
         try(InputStream inputStream = new ByteArrayInputStream(lineCaptcha.getImageBytes())) {
             url =  minIoUtil.upload(fileName, inputStream, "image/png");
+            String code = lineCaptcha.getCode(); // 正确的验证码
+            redisTemplate.opsForValue().set(fileName, code, 60, TimeUnit.SECONDS); // 将验证码保存到redis
         }
         return ResponseEntity.succ(url);
     }
