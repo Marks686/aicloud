e1cda2bcf2a182397ca3231aa61252e21d6f67c6
Marks686 <17829791810@163.com>
2024-06-30 09:13:47 +0800

    Minio集成

---
+++ aicloud/src/test/java/com/example/aicloud/util/MinIoUtilTest.java
@@ -0,0 +1,26 @@
+package com.example.aicloud.util;
+
+import io.minio.errors.*;
+import jakarta.annotation.Resource;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.io.*;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.sql.SQLOutput;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class MinIoUtilTest {
+    @Resource
+    private MinIoUtil  minIoUtil;
+    @Test
+    void upload() throws IOException, ServerException, InsufficientDataException, ErrorResponseException, NoSuchAlgorithmException, InvalidKeyException, InvalidResponseException, XmlParserException, InternalException {
+        File file = new File("d:\\xiaogou.jpg");
+        InputStream inputStream = new FileInputStream(file);
+        System.out.println(minIoUtil.upload("xiaogou.jpg",inputStream,"image/jpg"));
+    }
+}
