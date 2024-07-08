47ecfded83210e14902681e9345f88db6714ffc3
Marks686 <17829791810@163.com>
2024-07-08 19:19:41 +0800

    通义千问绘图功能实现

--- aicloud/src/main/java/org/spring/aicloud/controller/TongyiController.java
+++ aicloud/src/main/java/org/spring/aicloud/controller/TongyiController.java
@@ -4,6 +4,7 @@ import com.alibaba.cloud.ai.tongyi.chat.TongYiChatClient;
 import com.alibaba.cloud.ai.tongyi.chat.TongYiChatOptions;
 import com.alibaba.cloud.ai.tongyi.image.TongYiImagesClient;
 import com.alibaba.cloud.ai.tongyi.image.TongYiImagesOptions;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import jakarta.annotation.Resource;
 import org.spring.aicloud.entity.Answer;
 import org.spring.aicloud.service.IAnswerService;
@@ -16,6 +17,8 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/tongyi")
@@ -67,18 +70,57 @@ public class TongyiController {
      * @param question
      * @return
      */
+    /**
+     * 绘画
+     *
+     * @param question
+     * @return
+     */
     @RequestMapping("/draw")
     public ResponseEntity draw(String question) {
-        if (!StringUtils.hasLength(question)){
-            return ResponseEntity.fail("question is null");
+        if (!StringUtils.hasLength(question)) {
+            return ResponseEntity.fail("问题不能为空");
         }
+        String result = imageClient.call(new ImagePrompt(question))
+                .getResult().getOutput().getUrl();
+        Answer answer = new Answer();
+        answer.setTitle(question);
+        answer.setContent(result);
+        answer.setModel(2); // todo:后面优化成枚举
+        answer.setUid(SecurityUtil.getCurrentUser().getUid());
+        answer.setType(2); // todo:后面优化成枚举
+        boolean saveResult = answerService.save(answer);
+        if (saveResult) {
+            return ResponseEntity.succ(result);
+        }
+        return ResponseEntity.fail("操作失败，请重试！");
+    }
 
 
-        String  result = imageClient.call(new ImagePrompt(question,
-                TongYiImagesOptions.builder()
-                .withHeight(100)
-                .withWidth(100).build()
-        )).getResult().getOutput().getUrl();
-        return ResponseEntity.succ(result);
+    /**
+     * 获取聊天列表
+     */
+    @RequestMapping("/getchatlist")
+    public ResponseEntity getChatList() {
+        QueryWrapper<Answer> queryWrapper = new QueryWrapper<>();
+        queryWrapper.eq("uid", SecurityUtil.getCurrentUser().getUid());
+        queryWrapper.eq("type", 1);
+        queryWrapper.eq("model", 2);
+        queryWrapper.orderByDesc("aid");
+        List<Answer> list = answerService.list(queryWrapper);
+        return ResponseEntity.succ(list);
+    }
+
+    /**
+     * 获取绘画历史列表
+     */
+    @RequestMapping("/getdrawlist")
+    public ResponseEntity getDrawList() {
+        QueryWrapper<Answer> queryWrapper = new QueryWrapper<>();
+        queryWrapper.eq("model",2); // todo:后面优化成枚举
+        queryWrapper.eq("type",2); // todo:后面优化成枚举
+        queryWrapper.eq("uid",SecurityUtil.getCurrentUser().getUid());
+        queryWrapper.orderByDesc("aid");
+        return ResponseEntity.succ(answerService.list(queryWrapper));
     }
 }
